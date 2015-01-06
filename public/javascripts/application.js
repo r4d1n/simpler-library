cc05ba1789433dc7c6c3a3857fe950353d068461
Jared Radin <jared.radin@gmail.com>
2015-01-06 12:49:53 -0800

    working on list item view functions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,51 +4,11 @@ $(document).ready(function() {
   database.load();
 });
 
+
+
 $("#book-form").submit(function(event) {
   event.preventDefault();
   var formView = new BookFormView("#book-form", database);
   database.send(formView.getBook());
   formView.clear();
 });
-
-//manipulating the table, should be moved to ListItemView
-function listRow(el) {
-  return $(el).parents('tr');
-}
-
-function rowTitle(row) {
-  return $(row).find('.att-title').text();
-}
-
-$(".delete-button").click(function(event) {
-  event.preventDefault();
-  var row = listRow(this);
-  var title = rowTitle(row);
-  row.remove();
-  database.delete(title);
-});
-
-// (function isEditable() {
-//   $('.editable').click(function() {
-//     var $self = $(this);
-//     var value = $self.text();
-//     console.log(value);
-//     $self.html("<form><input id='edit-field'></form>")
-//     $('#edit-field').attr('value', value);
-//   })
-//   $('.editable').blur(function(){
-//     value = $('.edit-field').val();
-//     console.log(value)
-//     database.update(entry, value);
-//   })
-// }())
-//
-// function titleKey(el) {
-//   var title = rowTitle(listRow(this));
-//   var classes = $(el).attr("class").split();
-//   classes.filter(function(){
-//     for(var i=0; i < classes.length; i++)
-//   })
-//
-//   return { title : key }
-// }
--- public/javascripts/list-item-view.js
+++ public/javascripts/list-item-view.js
@@ -1,3 +1,52 @@
-var ListItemView = function(book) { // takes object returned by BookFormView getBook()
-  this.book = book;
+var ListItem = function(context) { // takes object returned by BookFormView getBook()
+  this.book = context;
+  this.isEditable();
+  this.hasDelete();
+}
+
+// Activate editable list
+ListItem.prototype.isEditable = function() {
+  $('.editable').click(function() {
+    var $self = $(this);
+    var $orig = $self.html();
+    var $value = $self.text();
+    $self.html("<form><input id='edit-field'></form>")
+    $('#edit-field').attr('value', $value);
+  })
+  $($self).blur(function(){
+    $value = $self.val();
+    $self.html($orig).val($value);
+    database.update(entry, $value);
+  })
+}
+
+//Manipulating table view
+ListItem.prototype.getRow = function(el) {
+  return $(el).parents('tr');
+}
+
+ListItem.prototype.getTitle = function(row) {
+  return $(row).find('.att-title').text();
+}
+
+ListItem.prototype.getKey = function(el) {
+  var title = rowTitle(listRow(this));
+  //   var classes = $(el).attr("class").split();
+  //   classes.filter(function(){
+  //     for(var i=0; i < classes.length; i++)
+  //   })
+  //
+  //   return { title : key }
+  // }
+  
+}
+
+ListItem.prototype.hasDelete = function() {
+  $(".delete-button").click(function(event) {
+    event.preventDefault();
+    var row = listRow(this);
+    var title = rowTitle(row);
+    row.remove();
+    database.delete(title);
+  });
 }
