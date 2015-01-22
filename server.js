3b3842ed9ce9939922da4fe26a9c4637b11edb3e
Jared Radin <jared.radin@gmail.com>
2015-01-21 22:18:58 -0800

    debugging port for heroku

--- server.js
+++ server.js
@@ -129,7 +129,8 @@ app.post('/books/update', function (req, res) {
   res.render('list', {'books' : booksDB});
 })
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
 
   var host = server.address().address
   var port = server.address().port
