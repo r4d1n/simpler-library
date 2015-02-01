1443ee251756142ceac64cde3a892e1e1da1476f
Jared Radin <jared.radin@gmail.com>
2015-02-01 14:38:23 -0800

    update DB migration with more fields

--- migrations/20150128211338_create_books_table.js
+++ migrations/20150128211338_create_books_table.js
@@ -4,6 +4,16 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('title');
     table.string('photographer');
+    table.string('nationality');
+    table.string('type');
+    table.string('genre');
+    table.string('textby');
+    table.string('publisher');
+    table.string('isbn');
+    table.string('year');
+    table.string('tags');
+    table.string('comments');
+    table.string('signed');
     table.timestamps();
   })
 };
