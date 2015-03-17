72fe922cfd77e1a7d4753b18a07f743a63d5c243
Jared Radin <jared.radin@gmail.com>
2015-03-17 10:28:28 -0700

    actually start book model....

--- models/book.js
+++ models/book.js
@@ -1,5 +1,20 @@
 var bookshelf = app.get('bookshelf');
 
-var Book = bookshelf.Model.extend({
-  
+var bookModel = bookshelf.Model.extend({
+  convertDate: function(integer) {
+    var date = new Date();
+    date.setTime(integer);
+    return date;
+  },
+  index: function(query, sortColumn) {
+    sortColumn = sortColumn || "id";
+    if (query) {
+      return knex('books').where('tags', 'like', "%" + knex.raw(query) + "%").orderBy(sortColumn, "desc")
+    } else {
+      return knex('books')
+      .join('users', 'books.user_id', 'users.id')
+      .orderBy(sortColumn, "desc")
+      .select('books.*', 'users.user_name');
+    }
+  }
 });
